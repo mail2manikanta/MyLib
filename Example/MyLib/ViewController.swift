cb9bf239c55ec0aa3cda0e257f04e9484a2e9581
Manikanta N <mail2manikanta@gmail.com>
2018-06-03 11:38:13 -0400

    Initial commit

---
+++ Example/MyLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyLib
+//
+//  Created by mail2manikanta@gmail.com on 06/03/2018.
+//  Copyright (c) 2018 mail2manikanta@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
